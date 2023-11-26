7b80c5c28fc3f93bb4391151d51e4fdeadc9221b
definitely-no-a-furry <HeijnisTies@outlook.com>
2023-11-26 18:24:21 +0100

    Done for testing

--- app/testingarea.js
+++ app/testingarea.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { Text, TouchableOpacity, SafeAreaView } from 'react-native'
 import { useRouter } from 'expo-router'
-import DText from '../components/DText'
 import { defaultDark } from '../themes/default'
 import { StatusBar } from 'expo-status-bar'
-import UserName from '../components/UserName'
-import SpoilerText from '../components/SpoilerText'
 
 const App = () => {
     const router = useRouter()
@@ -14,11 +11,6 @@ const App = () => {
             <StatusBar hidden={true}/>
             <TouchableOpacity style={[defaultDark.button, { margin: 7, padding: 5 }]} onPress={() => { router.push('/home') }}><Text style={{ color: '#fff' }}>Back</Text></TouchableOpacity>
             {/* everything from here is a testing area */}
-            <Text style={{ color: '#fff' }}>
-                <DText style={defaultDark}>"Hello":world</DText>
-            </Text>
-            <UserName userData={{ name: 'Amogus', level_string: 'Admin' }}/>
-            <SpoilerText>Hello!</SpoilerText>
         </SafeAreaView>
     )
 }
